45ac4f1ef6651d874d190af26cf2447d93cc8e92
Lawrence Duong <law.duong@berkeley.edu>
2021-04-22 19:11:50 -0700

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,32 @@
+
+
+
+// Source: https://www.w3schools.com/howto/howto_js_navbar_slide.asp
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("nav-bar").style.top = "0";
+  } else {
+    document.getElementById("nav-bar").style.top = "-100px";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+
+// Typing animation
+let greeting = document.getElementById('greeting');
+
+var typewriter = new Typewriter(greeting, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
+
