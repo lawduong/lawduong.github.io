6d549ecebfc1dc2f3c1d9cedf100e6223e6dada1
Lawrence Duong <law.duong@berkeley.edu>
2021-04-26 20:12:22 -0700

    First edition

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,12 +10,52 @@ function scrollFunction() {
 window.onscroll = function() {scrollFunction()};
 
 
-var i = 0;
+// var i = 0;
 
-function typeWriter(txt, id) {
-  if (i < txt.length) {
-    document.getElementById(id).innerHTML += txt.charAt(i);
-    i++;
-  } 
+// function typeWriter(txt, id) {
+//   if (i < txt.length) {
+//     document.getElementById(id).innerHTML += txt.charAt(i);
+//     i++;
+//   } 
+// };
+// window.onload = setInterval(function() {typeWriter("Hello there!", "greeting")}, 50);
+
+const words = ["Hello there!", "I'm Lawrence.", "Nice to meet you!"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('greeting').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 200);
+	};
+	loopTyping();
 };
-window.onload = setInterval(function() {typeWriter("Lawrence Duong", "greeting")}, 50);
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('greeting').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 100);
+	};
+	loopDeleting();
+};
+
+window.onload = function() {typingEffect()};
